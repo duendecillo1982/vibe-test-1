1cab791b21480fb8449a4cc1652212cc6b064165
Steven Vancoillie <duendecillo1982@gmail.com>
2026-04-22 14:32:52 +0200

    fix 404

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const STATIC_CACHE = "shopping-static-v2";
+const STATIC_CACHE = "shopping-static-v3";
 const STATIC_ASSETS = ["./", "./index.html", "./styles.css", "./script.js"];
 
 self.addEventListener("install", (event) => {
